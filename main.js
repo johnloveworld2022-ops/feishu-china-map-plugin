9f9762e50139ccb821230b30e442c1a7729ad73c
johnloveworld2022-ops <johnloveworld2022@gmail.com>
2025-08-25 01:53:36 -0700

    feat: add main.js as entry file

---
+++ main.js
@@ -0,0 +1,8 @@
+// main.js - Feishu China Map Plugin 入口文件
+// 后续地图逻辑可以放到这里
+import './style.css'
+
+document.querySelector('#root').innerHTML = `
+  <h1>Feishu China Map Plugin</h1>
+  <p>地图渲染逻辑请在这里补充</p>
+`
